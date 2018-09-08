0760c5c32eaef5af6a834f030231924180dd630b
BITTORIUM <42850689+bittorium@users.noreply.github.com>
2018-09-08 23:13:55 +0100

    Update Transfer.h

--- SimpleWallet/Transfer.h
+++ SimpleWallet/Transfer.h
@@ -1,6 +1,7 @@
 /*
 Copyright (C) 2018, The TurtleCoin developers
 Copyright (C) 2018, The PinkstarcoinV2 developers
+Copyright (C) 2018, The Bittorium developers
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by

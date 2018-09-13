561cc5fa48ba2be43b033506680dceded86e7d16
BITTORIUM <42850689+bittorium@users.noreply.github.com>
2018-09-13 15:43:47 +0100

    updated seed nodes

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -136,8 +136,8 @@ const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          //
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "";
 
 const char* const SEED_NODES[] = {
-  "",
-  "",
+  "95.216.187.5:34902",
+  "95.216.187.3:34902"
 };
 } // CryptoNote
 
